d621be20b8be7ff026ca4b5a253f5af395792b73
vlevko <vlevko@student.unit.ua>
2018-04-29 11:36:48 +0300

    Chapter_06

---
+++ Chapter_06/SubClass.java
@@ -0,0 +1,21 @@
+class SubClass extends SuperClass
+{
+	public static void main ( String[] args )
+	{
+		hello() ;
+		SuperClass.hello() ;
+		try
+		{
+			echo( args[0] ) ;
+		}
+		catch( Exception e )
+		{
+			System.out.println( "Требуется аргумент" ) ;
+		}
+	}
+
+	public static void hello()
+	{
+		System.out.println( "Привет из Подкласса" ) ;
+	}
+}
